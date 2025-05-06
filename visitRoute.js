07a8df7203a1055a4b25eed11601d7d8dbd12c1a
Milon Mete <milonmete07@gmail.com>
2025-05-06 16:51:56 +0530

    Add files via upload

---
+++ visitRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Visit = require('../models/Visit');
+
+router.get('/visits', async (req, res) => {
+  try {
+    const visit = await Visit.findOneAndUpdate(
+      {},
+      { $inc: { count: 1 } },
+      { upsert: true, new: true }
+    );
+    res.json({ count: visit.count });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
